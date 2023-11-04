4adbc46909ee3a44200a418826d0fa331cf2a2e3
David Andrs <andrsd@gmail.com>
2023-11-04 09:47:55 -0600

    FieldFunctionInterpolationLine is in 1 .h/.cc file

--- framework/mesh/FieldFunctionInterpolation/Line/chi_ffinter_line.cc
+++ framework/mesh/FieldFunctionInterpolation/Line/chi_ffinter_line.cc
@@ -1,17 +1,113 @@
 #include "chi_ffinter_line.h"
-
+#include "math/SpatialDiscretization/SpatialDiscretization.h"
+#include "math/VectorGhostCommunicator/vector_ghost_communicator.h"
 #include "physics/FieldFunction/fieldfunction_gridbased.h"
-
+#include "mesh/Cell/cell.h"
+#include "mesh/MeshContinuum/chi_meshcontinuum.h"
 #include "chi_runtime.h"
-#include "chi_mpi.h"
 #include "chi_log.h"
-
+#include "chi_mpi.h"
 #include <fstream>
 
-//###################################################################
-/***/
+namespace chi_mesh
+{
+
 void
-chi_mesh::FieldFunctionInterpolationLine::ExportPython(std::string base_name)
+chi_mesh::FieldFunctionInterpolationLine::Initialize()
+{
+  Chi::log.Log0Verbose1() << "Initializing line interpolator.";
+  //================================================== Check for empty FF-list
+  if (field_functions_.empty())
+    throw std::logic_error("Unassigned field function in line "
+                           "field function interpolator.");
+
+  //================================================== Create points;
+  const chi_mesh::Vector3 vif = pf_ - pi_;
+  delta_d_ = vif.Norm() / (number_of_points_ - 1);
+
+  const auto omega = vif.Normalized();
+
+  interpolation_points_.push_back(pi_);
+  for (int k = 1; k < (number_of_points_); k++)
+    interpolation_points_.push_back(pi_ + omega * delta_d_ * k);
+
+  //====================================================== Loop over contexts
+  const size_t num_ff = field_functions_.size();
+  for (size_t ff = 0; ff < num_ff; ff++)
+  {
+    ff_contexts_.emplace_back();
+    auto& ff_context = ff_contexts_.back();
+
+    ff_context.ref_ff = field_functions_[ff];
+    const auto& sdm = ff_context.ref_ff->GetSpatialDiscretization();
+    const auto& grid = sdm.Grid();
+
+    ff_context.interpolation_points_ass_cell.assign(number_of_points_, 0);
+    ff_context.interpolation_points_has_ass_cell.assign(number_of_points_, false);
+
+    //================================================== Find a home for each
+    //                                                   point
+    for (const auto& cell : grid.local_cells)
+    {
+      for (int p = 0; p < number_of_points_; p++)
+      {
+        const auto& point = interpolation_points_[p];
+        if (grid.CheckPointInsideCell(cell, point))
+        {
+          ff_context.interpolation_points_ass_cell[p] = cell.local_id_;
+          ff_context.interpolation_points_has_ass_cell[p] = true;
+        }
+      } // for point p
+    }   // for cell
+  }     // for ff
+
+  Chi::log.Log0Verbose1() << "Finished initializing interpolator.";
+}
+
+void
+FieldFunctionInterpolationLine::Execute()
+{
+  Chi::log.Log0Verbose1() << "Executing line interpolator.";
+  for (int ff = 0; ff < field_functions_.size(); ff++)
+  {
+    auto& ff_ctx = ff_contexts_[ff];
+    const auto& ref_ff = *ff_ctx.ref_ff;
+    const auto& sdm = ref_ff.GetSpatialDiscretization();
+    const auto& grid = sdm.Grid();
+
+    const auto& uk_man = ref_ff.GetUnknownManager();
+    const auto uid = 0;
+    const auto cid = ref_component_;
+
+    const auto field_data = ref_ff.GetGhostedFieldVector();
+
+    ff_ctx.interpolation_points_values.assign(number_of_points_, 0.0);
+    for (int p = 0; p < number_of_points_; ++p)
+    {
+      if (not ff_ctx.interpolation_points_has_ass_cell[p]) continue;
+
+      const auto cell_local_index = ff_ctx.interpolation_points_ass_cell[p];
+      const auto& cell = grid.local_cells[cell_local_index];
+      const auto& cell_mapping = sdm.GetCellMapping(cell);
+      const size_t num_nodes = cell_mapping.NumNodes();
+
+      std::vector<double> shape_function_vals(num_nodes, 0.0);
+      cell_mapping.ShapeValues(interpolation_points_[p], shape_function_vals);
+
+      double point_value = 0.0;
+      for (size_t i = 0; i < num_nodes; ++i)
+      {
+        const int64_t imap = sdm.MapDOFLocal(cell, i, uk_man, uid, cid);
+
+        point_value += shape_function_vals[i] * field_data[imap];
+      } // for node i
+      ff_ctx.interpolation_points_values[p] = point_value;
+    } // for p
+  }   // for ff
+}
+
+void
+FieldFunctionInterpolationLine::ExportPython(std::string base_name)
 {
   std::ofstream ofile;
 
@@ -155,3 +251,5 @@ chi_mesh::FieldFunctionInterpolationLine::ExportPython(std::string base_name)
   Chi::log.Log() << "Exported Python files for field func \"" << field_functions_[0]->TextName()
                  << "\" to base name \"" << base_name << "\" Successfully";
 }
+
+} // namespace chi_mesh
