a2e0f6f587485e8b7c07353d17a48d7ae3e301b1
David Andrs <andrsd@gmail.com>
2023-11-04 12:59:36 -0600

    SurfaceMesherPredefined is in 1 .h/.cc file

--- framework/mesh/SurfaceMesher/Predefined/surfmesher_predefined.cc
+++ framework/mesh/SurfaceMesher/Predefined/surfmesher_predefined.cc
@@ -1,10 +1,14 @@
 #include "surfmesher_predefined.h"
-
 #include "chi_runtime.h"
 #include "chi_log.h"
 
+namespace chi_mesh
+{
+
 void
-chi_mesh::SurfaceMesherPredefined::Execute()
+SurfaceMesherPredefined::Execute()
 {
   Chi::log.Log0Verbose1() << "SurfaceMesherPredefined executed";
 }
+
+} // namespace chi_mesh
