0a59f73a553de3ea5654b2ac544a800fa8422104
David Andrs <andrsd@gmail.com>
2023-11-04 14:28:50 -0600

    PieceWiseLinearSlabMapping is in 1 .h/.cc file

--- framework/math/SpatialDiscretization/CellMappings/PieceWiseLinear/PieceWiseLinearSlabMapping.cc
+++ framework/math/SpatialDiscretization/CellMappings/PieceWiseLinear/PieceWiseLinearSlabMapping.cc
@@ -1,9 +1,127 @@
 #include "PieceWiseLinearSlabMapping.h"
-
+#include "mesh/MeshContinuum/chi_meshcontinuum.h"
+#include "math/SpatialDiscretization/CellMappings/PieceWiseLinearBaseMapping.h"
 #include "math/SpatialDiscretization/FiniteElement/QuadraturePointData.h"
 
 namespace chi_math::cell_mapping
 {
+
+PieceWiseLinearSlabMapping::PieceWiseLinearSlabMapping(
+  const chi_mesh::Cell& slab_cell,
+  const chi_mesh::MeshContinuum& ref_grid,
+  const chi_math::QuadratureLine& volume_quadrature)
+  : PieceWiseLinearBaseMapping(ref_grid,
+                               slab_cell,
+                               2, // num_nodes
+                               MakeFaceNodeMapping(slab_cell)),
+    volume_quadrature_(volume_quadrature)
+{
+  v0i_ = slab_cell.vertex_ids_[0];
+  v1i_ = slab_cell.vertex_ids_[1];
+  v0_ = ref_grid_.vertices[v0i_];
+  const auto& v1 = ref_grid_.vertices[v1i_];
+
+  chi_mesh::Vector3 v01 = v1 - v0_;
+  h_ = v01.Norm();
+
+  normals_[0] = slab_cell.faces_[0].normal_;
+  normals_[1] = slab_cell.faces_[1].normal_;
+}
+
+double
+PieceWiseLinearSlabMapping::SlabShape(uint32_t index,
+                                      const chi_mesh::Vector3& qpoint,
+                                      bool on_surface,
+                                      uint32_t edge) const
+{
+  double xi = 0.0;
+  if (!on_surface) xi = qpoint.x;
+  else
+    xi = static_cast<double>(edge);
+
+  double value = 0.0;
+  if (index == 0) value = 1.0 - xi;
+  else if (index == 1)
+    value = xi;
+
+  return value;
+}
+
+double
+PieceWiseLinearSlabMapping::SlabGradShape(uint32_t index) const
+{
+  double value = 0.0;
+
+  if (index == 0) value = -1.0 / h_;
+  else
+    value = 1.0 / h_;
+
+  return value;
+}
+
+double
+PieceWiseLinearSlabMapping::ShapeValue(const int i, const chi_mesh::Vector3& xyz) const
+{
+  const auto& p0 = ref_grid_.vertices[v0i_];
+  const auto& p1 = ref_grid_.vertices[v1i_];
+  chi_mesh::Vector3 xyz_ref = xyz - p0;
+
+  chi_mesh::Vector3 v01 = p1 - p0;
+
+  double xi = v01.Dot(xyz_ref) / v01.Norm() / h_;
+
+  if ((xi >= -1.0e-6) and (xi <= 1.0 + 1.0e-6))
+  {
+    if (i == 0) return 1.0 - xi;
+    else
+      return xi;
+  } // if in cell
+
+  return 0.0;
+}
+
+void
+PieceWiseLinearSlabMapping::ShapeValues(const chi_mesh::Vector3& xyz,
+                                        std::vector<double>& shape_values) const
+{
+  shape_values.resize(num_nodes_, 0.0);
+  const auto& p0 = ref_grid_.vertices[v0i_];
+  const auto& p1 = ref_grid_.vertices[v1i_];
+  chi_mesh::Vector3 xyz_ref = xyz - p0;
+
+  chi_mesh::Vector3 v01 = p1 - p0;
+
+  double xi = v01.Dot(xyz_ref) / v01.Norm() / h_;
+
+  if ((xi >= -1.0e-6) and (xi <= 1.0 + 1.0e-6))
+  {
+    for (int i = 0; i < num_nodes_; i++)
+    {
+      if (i == 0) shape_values[i] = 1.0 - xi;
+      else
+        shape_values[i] = xi;
+    } // for dof
+
+    return;
+  } // if in cell
+}
+
+chi_mesh::Vector3
+PieceWiseLinearSlabMapping::GradShapeValue(const int i, const chi_mesh::Vector3& xyz) const
+{
+  if (i == 0) return chi_mesh::Vector3(0.0, 0.0, -1.0 / h_);
+  else
+    return chi_mesh::Vector3(0.0, 0.0, 1.0 / h_);
+}
+
+void
+PieceWiseLinearSlabMapping::GradShapeValues(const chi_mesh::Vector3& xyz,
+                                            std::vector<chi_mesh::Vector3>& gradshape_values) const
+{
+  gradshape_values.clear();
+  gradshape_values.emplace_back(GradShapeValue(0, xyz));
+  gradshape_values.emplace_back(GradShapeValue(1, xyz));
+}
 
 finite_element::VolumetricQuadraturePointData
 PieceWiseLinearSlabMapping::MakeVolumetricQuadraturePointData() const
