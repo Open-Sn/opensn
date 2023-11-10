ec680a8bdafb61b4756acf9cb11b6523c911fc08
William Daryl Hawkins <hawkins22@llnl.gov>
2023-11-09 18:36:17 -0800

    Restoring Lua mesh modifier code

--- framework/mesh/mesh_vector.cc
+++ framework/mesh/mesh_vector.cc
@@ -3,27 +3,6 @@
 namespace chi_mesh
 {
 
-TensorRank2Dim3
-Vector3::OTimes(const Vector3& that) const
-{
-  TensorRank2Dim3 new_t;
-  for (int i = 0; i < 3; ++i)
-    for (int j = 0; j < 3; ++j)
-      new_t[i](j) = this->operator[](i) * that[j];
-
-  return new_t;
-}
-
-Vector3
-Vector3::Dot(const TensorRank2Dim3& that) const
-{
-  Vector3 new_vec;
-  for (int i = 0; i < 3; ++i)
-    new_vec(i) = this->Dot(that.t[i]);
-
-  return new_vec;
-}
-
 Vector3
 operator*(const double value, const Vector3& that)
 {
