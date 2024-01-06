199044cfca68bb65e34da902da4e519d76168644
Zachary Hardy <zhardy@lanl.gov>
2024-01-05 17:50:09 -0700

    Added routine for boundary-wise, group-wise leakage

--- lua/modules/linear_bolzmann_solvers/discrete_ordinates_solver/lbs_do_lua_utils.h
+++ lua/modules/linear_bolzmann_solvers/discrete_ordinates_solver/lbs_do_lua_utils.h
@@ -4,7 +4,9 @@
 
 namespace opensnlua::lbs
 {
-/**Computes balance tables and prints it to the console.
+
+/**
+ * Computes balance tables and prints it to the console.
  *
  * \param SolverIndex int Handle to the solver for which the list is to be
  * obtained.
@@ -14,7 +16,8 @@ namespace opensnlua::lbs
  */
 int chiLBSComputeBalance(lua_State* L);
 
-/**Computes the leakage for the specified groupset and boundary id.
+/**
+ * Computes the leakage for the specified groupset and boundary id.
  *
  * \param SolverIndex int Handle to the solver.
  * \param GroupSetHandle int Handle to the groupset.
@@ -26,4 +29,20 @@ int chiLBSComputeBalance(lua_State* L);
  * \author Jan
  */
 int chiLBSComputeLeakage(lua_State* L);
+
+/**
+ * Computes the group-wise leakage on all boundaries.
+ *
+ * \param SolverIndex int Handle to the solver.
+ * \param BoundaryNames array List of boundary names. These must be the
+ *      standard boundary names used in OpenSn:
+ *      xmax, xmin, ymax, ymin, zmax, zmin
+ *
+ * \return A table mapping boundary names to group-wise leakage arrays.
+ *
+ * \ingroup LBSLuaFunctions
+ * \author Zachary K. Hardy
+ */
+int ComputeLeakage(lua_State* L);
+
 } // namespace opensnlua::lbs
