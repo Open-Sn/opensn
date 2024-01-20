982a1d159e2c7e6d9e2d00e682a488fb45a13a0d
Zachary Hardy <zhardy@lanl.gov>
2024-01-19 17:31:04 -0700

    Updated tests

--- test/modules/linear_boltzmann_solvers/transport_adjoint/adjoint_2d_1c_response.lua
+++ test/modules/linear_boltzmann_solvers/transport_adjoint/adjoint_2d_1c_response.lua
@@ -3,100 +3,92 @@
 -- Test: Inner-product=1.38405e-05
 num_procs = 4
 
-
-
-
-
 --############################################### Check num_procs
-if (check_num_procs==nil and chi_number_of_processes ~= num_procs) then
-    chiLog(LOG_0ERROR,"Incorrect amount of processors. " ..
-                      "Expected "..tostring(num_procs)..
-                      ". Pass check_num_procs=false to override if possible.")
+if (check_num_procs == nil and chi_number_of_processes ~= num_procs) then
+    chiLog(LOG_0ERROR, "Incorrect amount of processors. " ..
+        "Expected " .. tostring(num_procs) ..
+        ". Pass check_num_procs=false to override if possible.")
     os.exit(false)
 end
 
 --############################################### Setup mesh
-nodes={}
-N=60
-L=5.0
-ds=L/N
-xmin=0.0
-for i=0,N do
-    nodes[i+1] = xmin + i*ds
+N = 60
+L = 5.0
+ds = L / N
+
+nodes = {}
+for i = 0, N do
+    nodes[i + 1] = i * ds
 end
-meshgen1 = chi_mesh.OrthogonalMeshGenerator.Create({ node_sets = {nodes,nodes} })
-chi_mesh.MeshGenerator.Execute(meshgen1)
+meshgen = chi_mesh.OrthogonalMeshGenerator.Create({ node_sets = { nodes, nodes } })
+chi_mesh.MeshGenerator.Execute(meshgen)
 
-----############################################### Set Material IDs
-NewRPP = chi_mesh.RPPLogicalVolume.Create
-vol0 = NewRPP({infx=true, infy=true, infz=true})
-chiVolumeMesherSetProperty(MATID_FROMLOGICAL,vol0,0)
+--############################################### Set Material IDs
+vol0 = chi_mesh.RPPLogicalVolume.Create({ infx = true,
+                                          infy = true,
+                                          infz = true })
+chiVolumeMesherSetProperty(MATID_FROMLOGICAL, vol0, 0)
 
-vol1 = NewRPP({ymin=0.0,ymax=0.8*L,infx=true,infz=true})
-chiVolumeMesherSetProperty(MATID_FROMLOGICAL,vol1,1)
+vol1 = chi_mesh.RPPLogicalVolume.Create({ ymin = 0.0, ymax = 0.8 * L,
+                                          infx = true,
+                                          infz = true })
+chiVolumeMesherSetProperty(MATID_FROMLOGICAL, vol1, 1)
 
+vol0b = chi_mesh.RPPLogicalVolume.Create({ xmin = -0.166666 + 2.5, xmax = 0.166666 + 2.5,
+                                           infy = true,
+                                           infz = true })
+chiVolumeMesherSetProperty(MATID_FROMLOGICAL, vol0b, 0)
 
+vol2 = chi_mesh.RPPLogicalVolume.Create({ xmin = -0.166666 + 2.5, xmax = 0.166666 + 2.5,
+                                          ymin = 0.0, ymax = 2 * 0.166666,
+                                          infz = true })
+chiVolumeMesherSetProperty(MATID_FROMLOGICAL, vol2, 2)
 
-----############################################### Set Material IDs
-vol0b = NewRPP({xmin=-0.166666+2.5,xmax=0.166666+2.5,infy=true,infz=true})
-chiVolumeMesherSetProperty(MATID_FROMLOGICAL,vol0b,0)
-
-vol2 = NewRPP({xmin=-0.166666+2.5,xmax=0.166666+2.5,ymin=0.0,ymax=2*0.166666,infz=true})
-chiVolumeMesherSetProperty(MATID_FROMLOGICAL,vol2,2)
-
-vol1b = NewRPP({xmin=-1+2.5,xmax=1+2.5,ymin=0.9*L,ymax=L,infz=true})
-chiVolumeMesherSetProperty(MATID_FROMLOGICAL,vol1b,1)
+vol1b = chi_mesh.RPPLogicalVolume.Create({ xmin = -1 + 2.5, xmax = 1 + 2.5,
+                                           ymin = 0.9 * L, ymax = L,
+                                           infz = true })
+chiVolumeMesherSetProperty(MATID_FROMLOGICAL, vol1b, 1)
 
 
 --############################################### Add materials
+num_groups = 1
+
 materials = {}
 materials[1] = chiPhysicsAddMaterial("Test Material");
 materials[2] = chiPhysicsAddMaterial("Test Material2");
 materials[3] = chiPhysicsAddMaterial("Test Material3");
 
-chiPhysicsMaterialAddProperty(materials[1],TRANSPORT_XSECTIONS)
-chiPhysicsMaterialAddProperty(materials[2],TRANSPORT_XSECTIONS)
-chiPhysicsMaterialAddProperty(materials[3],TRANSPORT_XSECTIONS)
+-- Cross sections
+chiPhysicsMaterialAddProperty(materials[1], TRANSPORT_XSECTIONS)
+chiPhysicsMaterialSetProperty(materials[1], TRANSPORT_XSECTIONS,
+                              SIMPLEXS1, 1, 0.01, 0.01)
 
-chiPhysicsMaterialAddProperty(materials[1],ISOTROPIC_MG_SOURCE)
-chiPhysicsMaterialAddProperty(materials[2],ISOTROPIC_MG_SOURCE)
-chiPhysicsMaterialAddProperty(materials[3],ISOTROPIC_MG_SOURCE)
+chiPhysicsMaterialAddProperty(materials[2], TRANSPORT_XSECTIONS)
+chiPhysicsMaterialSetProperty(materials[2], TRANSPORT_XSECTIONS,
+                              SIMPLEXS1, 1, 0.1 * 20, 0.8)
 
+chiPhysicsMaterialAddProperty(materials[3], TRANSPORT_XSECTIONS)
+chiPhysicsMaterialSetProperty(materials[3], TRANSPORT_XSECTIONS,
+                              SIMPLEXS1, num_groups, 0.3 * 20, 0.0)
 
-num_groups = 1
-chiPhysicsMaterialSetProperty(materials[1],
-                              TRANSPORT_XSECTIONS,
-                              SIMPLEXS1,1,0.01,0.01)
-chiPhysicsMaterialSetProperty(materials[2],
-                              TRANSPORT_XSECTIONS,
-                              SIMPLEXS1,1,0.1*20,0.8)
-chiPhysicsMaterialSetProperty(materials[3],
-                              TRANSPORT_XSECTIONS,
-                              SIMPLEXS1,1,0.3*20,0.0)
-
-src={}
-for g=1,num_groups do
-    src[g] = 0.0
+-- Sources
+src = {}
+for g = 1, num_groups do
+    if g == 1 then src[g] = 3.0 else src[g] = 0.0 end
 end
-src[1] = 0.0
-chiPhysicsMaterialSetProperty(materials[1],ISOTROPIC_MG_SOURCE,FROM_ARRAY,src)
-src[1] = 0.0
-chiPhysicsMaterialSetProperty(materials[2],ISOTROPIC_MG_SOURCE,FROM_ARRAY,src)
-src[1] = 3.0
-chiPhysicsMaterialSetProperty(materials[3],ISOTROPIC_MG_SOURCE,FROM_ARRAY,src)
+chiPhysicsMaterialAddProperty(materials[3], ISOTROPIC_MG_SOURCE)
+chiPhysicsMaterialSetProperty(materials[3], ISOTROPIC_MG_SOURCE, FROM_ARRAY, src)
 
 
 --############################################### Setup Physics
-pquad0 = chiCreateProductQuadrature(GAUSS_LEGENDRE_CHEBYSHEV,48, 6)
-chiOptimizeAngularQuadratureForPolarSymmetry(pquad0, 4.0*math.pi)
+pquad0 = chiCreateProductQuadrature(GAUSS_LEGENDRE_CHEBYSHEV, 48, 6)
+chiOptimizeAngularQuadratureForPolarSymmetry(pquad0, 4.0 * math.pi)
 
-lbs_block =
-{
+lbs_block = {
     num_groups = num_groups,
-    groupsets =
-    {
+    groupsets = {
         {
-            groups_from_to = {0, num_groups-1},
+            groups_from_to = { 0, num_groups - 1 },
             angular_quadrature_handle = pquad0,
             inner_linear_method = "gmres",
             l_abs_tol = 1.0e-6,
@@ -106,49 +98,36 @@ lbs_block =
     }
 }
 
-lbs_options =
-{
-    scattering_order = 1,
-}
+lbs_options = { scattering_order = 1 }
 
---############################################### Initialize and Execute Solver
-phys1 = lbs.DiscreteOrdinatesAdjointSolver.Create(lbs_block)
-lbs.SetOptions(phys1, lbs_options)
+phys = lbs.DiscreteOrdinatesAdjointSolver.Create(lbs_block)
+lbs.SetOptions(phys, lbs_options)
 
---############################################### Create QOIs
-tvol0 = NewRPP({xmin=2.3333,xmax=2.6666,ymin=4.16666,ymax=4.33333,infz=true})
-tvol1 = NewRPP({xmin=0.5   ,xmax=0.8333,ymin=4.16666,ymax=4.33333,infz=true})
+--############################################### Create QoI region
+qoi_vol = chi_mesh.RPPLogicalVolume.Create({ xmin = 0.5, xmax = 0.8333,
+                                             ymin = 4.16666, ymax = 4.33333,
+                                             infz = true })
 
-chiAdjointSolverAddResponseFunction(phys1,"QOI0",tvol0)
-chiAdjointSolverAddResponseFunction(phys1,"QOI1",tvol1)
-chiSolverSetBasicOption(phys1, "REFERENCE_RF", "QOI1")
+chiAdjointSolverAddResponseFunction(phys, "QoI", qoi_vol)
+chiSolverSetBasicOption(phys, "REFERENCE_RF", "QoI")
 
-ss_solver = lbs.SteadyStateSolver.Create({lbs_solver_handle = phys1})
+--############################################### Initialize and Compute Response
+ss_solver = lbs.SteadyStateSolver.Create({lbs_solver_handle = phys})
 
 chiSolverInitialize(ss_solver)
---chiSolverExecute(ss_solver)
 
-chiLBSReadFluxMoments(phys1, "Adjoint2D_1b_adjoint")
-value = chiAdjointSolverComputeInnerProduct(phys1)
+chiLBSReadFluxMoments(phys, "Adjoint2D_1b_adjoint")
+value = chiAdjointSolverComputeInnerProduct(phys)
 chiLog(LOG_0,string.format("Inner-product=%.5e", value))
-
---############################################### Get field functions
-ff_m0 = chiGetFieldFunctionHandleByName("phi_g000_m00")
-ff_m1 = chiGetFieldFunctionHandleByName("phi_g000_m01")
-ff_m2 = chiGetFieldFunctionHandleByName("phi_g000_m02")
-
-
---############################################### Slice plot
-
---############################################### Volume integrations
-
-
 
 --############################################### Exports
 if master_export == nil then
+    ff_m0 = chiGetFieldFunctionHandleByName("phi_g000_m00")
+    ff_m1 = chiGetFieldFunctionHandleByName("phi_g000_m01")
+    ff_m2 = chiGetFieldFunctionHandleByName("phi_g000_m02")
+
     chiExportMultiFieldFunctionToVTK({ff_m0, ff_m1, ff_m2},"ZPhi_LBAdjointResponse")
 end
-
 
 --############################################### Cleanup
 chiMPIBarrier()
--- test/modules/linear_boltzmann_solvers/transport_adjoint/adjoint_2d_3a_forward.lua
+++ test/modules/linear_boltzmann_solvers/transport_adjoint/adjoint_2d_3a_forward.lua
@@ -14,88 +14,82 @@
 --  QOI-value[sum]= 2.21354e-05
 num_procs = 4
 
-
-
-
-
 --############################################### Check num_procs
-if (check_num_procs==nil and chi_number_of_processes ~= num_procs) then
-    chiLog(LOG_0ERROR,"Incorrect amount of processors. " ..
-                      "Expected "..tostring(num_procs)..
-                      ". Pass check_num_procs=false to override if possible.")
+if (check_num_procs == nil and chi_number_of_processes ~= num_procs) then
+    chiLog(LOG_0ERROR, "Incorrect amount of processors. " ..
+        "Expected " .. tostring(num_procs) ..
+        ". Pass check_num_procs=false to override if possible.")
     os.exit(false)
 end
 
 --############################################### Setup mesh
-nodes={}
-N=60
-L=5.0
-ds=L/N
-xmin=0.0
-for i=0,N do
-    nodes[i+1] = xmin + i*ds
+N = 60
+L = 5.0
+ds = L / N
+
+nodes = {}
+for i = 0, N do
+    nodes[i + 1] = i * ds
 end
-meshgen1 = chi_mesh.OrthogonalMeshGenerator.Create({ node_sets = {nodes,nodes} })
-chi_mesh.MeshGenerator.Execute(meshgen1)
+meshgen = chi_mesh.OrthogonalMeshGenerator.Create({ node_sets = { nodes, nodes } })
+chi_mesh.MeshGenerator.Execute(meshgen)
 
-----############################################### Set Material IDs
-NewRPP = chi_mesh.RPPLogicalVolume.Create
-vol0 = NewRPP({infx=true, infy=true, infz=true})
-vol1 = NewRPP({ymin=0.0,ymax=0.8*L,infx=true,infz=true})
-chiVolumeMesherSetProperty(MATID_FROMLOGICAL,vol0,0)
-chiVolumeMesherSetProperty(MATID_FROMLOGICAL,vol1,1)
+--############################################### Set Material IDs
+vol0 = chi_mesh.RPPLogicalVolume.Create({ infx = true,
+                                          infy = true,
+                                          infz = true })
+chiVolumeMesherSetProperty(MATID_FROMLOGICAL, vol0, 0)
 
+vol1 = chi_mesh.RPPLogicalVolume.Create({ infx = true,
+                                          ymin = 0.0, ymax = 0.8 * L,
+                                          infz = true })
+chiVolumeMesherSetProperty(MATID_FROMLOGICAL, vol1, 1)
 
+vol0b = chi_mesh.RPPLogicalVolume.Create({ xmin = -0.166666 + 2.5, xmax = 0.166666 + 2.5,
+                                           infy = true,
+                                           infz = true })
+chiVolumeMesherSetProperty(MATID_FROMLOGICAL, vol0b, 0)
 
-----############################################### Set Material IDs
-vol0b = NewRPP({xmin=-0.166666+2.5,xmax=0.166666+2.5,infy=true,infz=true})
-vol1b = NewRPP({xmin=-1+2.5,xmax=1+2.5,ymin=0.9*L,ymax=L,infz=true})
-chiVolumeMesherSetProperty(MATID_FROMLOGICAL,vol0b,0)
-chiVolumeMesherSetProperty(MATID_FROMLOGICAL,vol1b,1)
+vol1b = chi_mesh.RPPLogicalVolume.Create({ xmin = -1 + 2.5, xmax = 1 + 2.5,
+                                           ymin = 0.9 * L, ymax = L,
+                                           infz = true })
+chiVolumeMesherSetProperty(MATID_FROMLOGICAL, vol1b, 1)
 
 
 --############################################### Add materials
+num_groups = 10
+
 materials = {}
-materials[1] = chiPhysicsAddMaterial("Test Material");
+materials[1] = chiPhysicsAddMaterial("Test Material1");
 materials[2] = chiPhysicsAddMaterial("Test Material2");
 
-chiPhysicsMaterialAddProperty(materials[1],TRANSPORT_XSECTIONS)
-chiPhysicsMaterialAddProperty(materials[2],TRANSPORT_XSECTIONS)
+chiPhysicsMaterialAddProperty(materials[1], TRANSPORT_XSECTIONS)
+chiPhysicsMaterialSetProperty(materials[1], TRANSPORT_XSECTIONS,
+                              SIMPLEXS1, num_groups, 0.01, 0.01)
 
-chiPhysicsMaterialAddProperty(materials[1],ISOTROPIC_MG_SOURCE)
-chiPhysicsMaterialAddProperty(materials[2],ISOTROPIC_MG_SOURCE)
+chiPhysicsMaterialAddProperty(materials[2], TRANSPORT_XSECTIONS)
+chiPhysicsMaterialSetProperty(materials[2], TRANSPORT_XSECTIONS,
+                              SIMPLEXS1, num_groups, 0.1 * 20, 0.8)
 
-
-num_groups = 10
-chiPhysicsMaterialSetProperty(materials[1],
-                              TRANSPORT_XSECTIONS,
-                              SIMPLEXS1,num_groups,0.01,0.01)
-chiPhysicsMaterialSetProperty(materials[2],
-                              TRANSPORT_XSECTIONS,
-                              SIMPLEXS1,num_groups,0.1*20,0.8)
-
-src={}
-for g=1,num_groups do
-    src[g] = 0.0
+-- Sources
+src = {}
+for g = 1, num_groups do
+    if g == 1 then src[g] = 1.0 else src[g] = 0.0 end
 end
-src[1] = 0.0
-chiPhysicsMaterialSetProperty(materials[1],ISOTROPIC_MG_SOURCE,FROM_ARRAY,src)
-src[1] = 0.0
-chiPhysicsMaterialSetProperty(materials[2],ISOTROPIC_MG_SOURCE,FROM_ARRAY,src)
-src[1] = 1.0
+
+loc = { 1.25 - 0.5 * ds, 1.5 * ds, 0.0 }
+pt_src = lbs.PointSource.Create({ location = loc, strength = src })
 
 --############################################### Setup Physics
-pquad0 = chiCreateProductQuadrature(GAUSS_LEGENDRE_CHEBYSHEV,12, 2)
-chiOptimizeAngularQuadratureForPolarSymmetry(pquad0, 4.0*math.pi)
+pquad = chiCreateProductQuadrature(GAUSS_LEGENDRE_CHEBYSHEV, 12, 2)
+chiOptimizeAngularQuadratureForPolarSymmetry(pquad, 4.0 * math.pi)
 
-lbs_block =
-{
+lbs_block = {
     num_groups = num_groups,
-    groupsets =
-    {
+    groupsets = {
         {
-            groups_from_to = {0, num_groups-1},
-            angular_quadrature_handle = pquad0,
+            groups_from_to = { 0, num_groups - 1 },
+            angular_quadrature_handle = pquad,
             inner_linear_method = "gmres",
             l_abs_tol = 1.0e-6,
             l_max_its = 500,
@@ -104,28 +98,24 @@ lbs_block =
     }
 }
 
-lbs_options =
-{
+lbs_options = {
     scattering_order = 1,
+    point_sources = {pt_src}
 }
 
-phys1 = lbs.DiscreteOrdinatesSolver.Create(lbs_block)
-lbs.SetOptions(phys1, lbs_options)
-
---############################################### Add point source
-chiLBSAddPointSource(phys1, 1.25 - 0.5*ds, 1.5*ds, 0.0, src)
+phys = lbs.DiscreteOrdinatesSolver.Create(lbs_block)
+lbs.SetOptions(phys, lbs_options)
 
 --############################################### Initialize and Execute Solver
-ss_solver = lbs.SteadyStateSolver.Create({lbs_solver_handle = phys1})
+ss_solver = lbs.SteadyStateSolver.Create({ lbs_solver_handle = phys })
 
 chiSolverInitialize(ss_solver)
 chiSolverExecute(ss_solver)
 
---############################################### Create QOIs
-tvol0 = NewRPP({xmin=2.3333,xmax=2.6666,ymin=4.16666,ymax=4.33333,infz=true})
-tvol1 = NewRPP({xmin=0.5   ,xmax=0.8333,ymin=4.16666,ymax=4.33333,infz=true})
-
-
+--############################################### Create QoI Region
+qoi_vol = chi_mesh.RPPLogicalVolume.Create({ xmin = 0.5, xmax = 0.8333,
+                                             ymin = 4.16666, ymax = 4.33333,
+                                             infz = true })
 
 --############################################### Get field functions
 ff_m0 = chiGetFieldFunctionHandleByName("phi_g000_m00")
@@ -136,29 +126,28 @@ ff_m2 = chiGetFieldFunctionHandleByName("phi_g000_m02")
 --############################################### Slice plot
 
 --############################################### Volume integrations
-QOI_value_sum = 0.0
-for g=0,num_groups-1 do
-    ff = chiGetFieldFunctionHandleByName("phi_g"..
-            string.format("%03d",g).."_m"..string.format("%02d",0))
-    ffi1 = chiFFInterpolationCreate(VOLUME)
-    curffi = ffi1
-    chiFFInterpolationSetProperty(curffi,OPERATION,OP_SUM)
-    chiFFInterpolationSetProperty(curffi,LOGICAL_VOLUME,tvol1)
-    chiFFInterpolationSetProperty(curffi,ADD_FIELDFUNCTION,ff)
+sum = 0.0
+for g = 0, num_groups - 1 do
+    ff = chiGetFieldFunctionHandleByName(
+        "phi_g" .. string.format("%03d", g) .. "_m" .. string.format("%02d", 0))
+    ffi = chiFFInterpolationCreate(VOLUME)
+    chiFFInterpolationSetProperty(ffi, OPERATION, OP_SUM)
+    chiFFInterpolationSetProperty(ffi, LOGICAL_VOLUME, qoi_vol)
+    chiFFInterpolationSetProperty(ffi, ADD_FIELDFUNCTION, ff)
 
-    chiFFInterpolationInitialize(curffi)
-    chiFFInterpolationExecute(curffi)
-    QOI_value = chiFFInterpolationGetValue(curffi)
+    chiFFInterpolationInitialize(ffi)
+    chiFFInterpolationExecute(ffi)
+    value = chiFFInterpolationGetValue(ffi)
 
-    chiLog(LOG_0,string.format("QOI-value["..tostring(g).."]= %.5e", QOI_value))
+    chiLog(LOG_0, string.format("QoI-value[" .. tostring(g) .. "]= %.5e", value))
 
-    QOI_value_sum = QOI_value_sum + QOI_value
+    sum = sum + value
 end
-chiLog(LOG_0,string.format("QOI-value[sum]= %.5e", QOI_value_sum))
+chiLog(LOG_0, string.format("QoI-value[sum]= %.5e", sum))
 
 --############################################### Exports
 if master_export == nil then
-    chiExportMultiFieldFunctionToVTK({ff_m0, ff_m1, ff_m2},"ZPhi_LBS")
+    chiExportMultiFieldFunctionToVTK({ ff_m0, ff_m1, ff_m2 }, "ZPhi_LBS")
 end
 
 --[0]  QOI-vallue[0]= 1.95637e-09
