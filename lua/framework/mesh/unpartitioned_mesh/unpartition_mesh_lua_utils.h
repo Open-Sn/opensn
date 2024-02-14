ee481b8e4a501223a52c1f0b6eee336604a9ad3b
David Andrs <andrsd@gmail.com>
2024-02-14 10:37:16 -0700

    Removing `UnpartitionedMeshUploadCell`
    Not used. No need for this.

--- lua/framework/mesh/unpartitioned_mesh/unpartition_mesh_lua_utils.h
+++ lua/framework/mesh/unpartitioned_mesh/unpartition_mesh_lua_utils.h
@@ -210,63 +210,6 @@ int UnpartitionedMeshFromMshFormat(lua_State* L);
  */
 int UnpartitionedMeshFromExodusII(lua_State* L);
 
-/**Uploads a cell
- *
- * \param handle int Handle to mesh.
- * \param cell_table lua_table A Lua-table containing fields of data. See
- *  cell_table below.
- *
- * ## _
- *
- * ###cell_table
- * A lua-table with the following fields:
- * - `type` <I>string</I>. The value of this field contains the cell's primary
- * type. The value can be "SLAB", "POLYGON" or "POLYHEDRON".
- * - `sub_type` <I>string</I>. The value of this field constains the cell's
- *  secondary type. The value can be "SLAB", "POLYGON", "TRIANGLE",
- *  "QUADRILATERAL", "POLYHEDRON", "TETRAHEDRON" or "HEXAHEDRON".
- * - `num_faces` <I>int</I>. The value of this field represent the number of faces
- *  specified for this cell. Each face is contained in a field "faceX" where X is
- *  the face index.
- * - `material_id` <I>int</I>. (Optional) The value of this field holds a material
- *  identifier. If not provided, will be defaulted to -1.
- * - `faceX` <I>table</I>. A field holding a lua-table containing the vertex-ids
- *  of face X. There must be `num_faces` of these fields, i.e., "face0", "face1",
- * etc.
- *
- * ###Example
- * Example usage
- * \code
- * UnpartitionedMeshUploadVertex(umesh, 0, 0, 0)
- * UnpartitionedMeshUploadVertex(umesh, 1, 0, 0)
- * UnpartitionedMeshUploadVertex(umesh, 1, 1, 0)
- * UnpartitionedMeshUploadVertex(umesh, 0, 1, 0)
- *
- * UnpartitionedMeshUploadVertex(umesh, 0, 0, 1)
- * UnpartitionedMeshUploadVertex(umesh, 1, 0, 1)
- * UnpartitionedMeshUploadVertex(umesh, 1, 1, 1)
- * UnpartitionedMeshUploadVertex(umesh, 0, 1, 1)
- *
- * cell = {}
- * cell.type        = "POLYHEDRON"
- * cell.sub_type    = "HEXAHEDRON"
- * cell.num_faces   = 6
- * cell.material_id = 0
- * cell.face0 = {1,2,6,5}
- * cell.face1 = {0,4,7,3}
- * cell.face2 = {2,3,7,6}
- * cell.face3 = {0,1,5,4}
- * cell.face4 = {4,5,6,7}
- * cell.face5 = {0,3,2,1}
- *
- * UnpartitionedMeshUploadCell(umesh, cell, true)
- * UnpartitionedMeshFinalizeEmpty(umesh)
- * \endcode
- *
- * \ingroup LuaUnpartitionedMesh
- */
-int UnpartitionedMeshUploadCell(lua_State* L);
-
 /**Finalizes a mesh. This usually involves computing centroids and
  * establishing connectivity.
  *
