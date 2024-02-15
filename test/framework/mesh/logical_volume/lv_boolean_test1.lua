daa1f42fc9af30fd480fbbe18fb9f84d4a5a0aa2
ragusa <jean.ragusa@tamu.edu>
2024-02-14 22:58:21 -0600

    adding tests

--- test/framework/mesh/logical_volume/lv_boolean_test1.lua
+++ test/framework/mesh/logical_volume/lv_boolean_test1.lua
@@ -1,3 +1,10 @@
+--[[# test for boolean operations on logical volumes
+lv1 = sphere
+lv2 = right circular cylinder (rcc)
+lv3 = in rcc but not in sphere
+--]]
+
+-- set up orthogonal 3D geometry
 nodes={}
 N=40
 L=5
@@ -13,16 +20,29 @@ meshgen1 = mesh.OrthogonalMeshGenerator.Create
 })
 mesh.MeshGenerator.Execute(meshgen1)
 
+-- assign matID 10 to all cells
+vol0 = mesh.RPPLogicalVolume.Create({infx=true, infy=true, infz=true})
+VolumeMesherSetProperty(MATID_FROMLOGICAL,vol0,10)
+
+-- create logical volume lv1 as an analytical sphere
 lv1 = mesh.SphereLogicalVolume.Create({r = 1.3, x=1.0, y=-1.0, z=2.0})
 
+-- create logical volume lv2 as an analytical rcc
 lv2 = mesh.RCCLogicalVolume.Create({r = 1.3,
                                         x0=-0.8, y0=-0.8, z0=-1.5,
                                         vx=1.0, vy=1.0, vz=3.0})
+
+-- create logical volume lv3 as boolean: true if cell is in lv2 and false if in lv1
 lv3 = mesh.BooleanLogicalVolume.Create
 ({
   parts = { { op=true, lv=lv2 },
             { op=false, lv=lv1 } }
 })
+-- assign matID 1 to all cells in lv3 which is the part of lv2 that is not in lv1
 VolumeMesherSetProperty(MATID_FROMLOGICAL, lv3, 1)
 
+-- assign matID 5 to all cells in lv1
+VolumeMesherSetProperty(MATID_FROMLOGICAL, lv1, 5)
+
+-- export to vtk
 MeshHandlerExportMeshToVTK("lv_boolean_test1")
---
+++ test/framework/mesh/logical_volume/lv_lua_func.lua
@@ -0,0 +1,37 @@
+--[[# test for lus function used as a logical volume
+--]]
+
+-- set up orthogonal 3D geometry
+nodes={}
+N=50
+L=5.0
+xmin = -L/2
+dx = L/N
+for i=1,(N+1) do
+  k=i-1
+  nodes[i] = xmin + k*dx
+end
+
+meshgen = mesh.OrthogonalMeshGenerator.Create
+({
+  node_sets = {nodes,nodes,nodes},
+})
+mesh.MeshGenerator.Execute(meshgen)
+
+-- assign mat ID 10 to whole domain
+vol0 = mesh.RPPLogicalVolume.Create({infx=true, infy=true, infz=true})
+VolumeMesherSetProperty(MATID_FROMLOGICAL,vol0,10)
+
+--Sets lua function describing a sphere (material 11)
+function MatIDFunction1(x,y,z,cur_id)
+    if (x*x + y*y + z*z < 1.0) then
+        return 11
+    end
+    return cur_id
+end
+-- assign mat ID 11 to lv using lua function
+VolumeMesherSetProperty(MATID_FROM_LUA_FUNCTION, "MatIDFunction1")
+
+-- export to vtk
+MeshHandlerExportMeshToVTK("lv_lua_func_out")
+
