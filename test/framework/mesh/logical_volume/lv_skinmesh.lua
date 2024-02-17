ea17692e80733cba060c5402a6ca5964aaac9a34
ragusa <jean.ragusa@tamu.edu>
2024-02-17 11:42:54 -0600

    fixing logical volume to work with skin mesh

---
+++ test/framework/mesh/logical_volume/lv_skinmesh.lua
@@ -0,0 +1,39 @@
+-- test for skin (surface) mesh used as a delimiter for a logical volume
+-- set up orthogonal 3D geometry
+nodes={}
+N=50
+L=5.0
+xmin = -L/2
+dx = L/N
+for i=1,(N+1) do
+  k=i-1
+  nodes[i] = xmin + k*dx
+end
+
+meshgen = mesh.OrthogonalMeshGenerator.Create
+({
+  node_sets = {nodes,nodes,nodes},
+})
+mesh.MeshGenerator.Execute(meshgen)
+
+-- assign mat ID 10 to whole domain
+vol0 = mesh.RPPLogicalVolume.Create({infx=true, infy=true, infz=true})
+mesh.SetProperty(MATID_FROMLOGICAL,vol0,10)
+
+-- create a logical volume as an analytical RPP
+vol1 = mesh.RPPLogicalVolume.Create
+({ xmin=-0.5,xmax=0.5,ymin=0.8,ymax=1.5, zmin=-1.5,zmax=0.5,  })
+-- assign mat ID 11 to lv of RPP
+mesh.SetProperty(MATID_FROMLOGICAL,vol1,11)
+
+-- create a logical volume as the interior of a skin mesh
+surfmesh = SurfaceMeshCreate()
+skin_mesh_file = "./cube_with_normals.obj"
+SurfaceMeshImportFromOBJFile(surfmesh, skin_mesh_file)
+lv_skinmesh = mesh.SurfaceMeshLogicalVolume.Create({surface_mesh_handle=surfmesh})
+-- assign mat ID 15 to lv of skin mesh
+mesh.SetProperty(MATID_FROMLOGICAL,lv_skinmesh,15)
+
+-- export to vtk
+mesh.ExportToVTK("lv_skinmesh_out")
+
