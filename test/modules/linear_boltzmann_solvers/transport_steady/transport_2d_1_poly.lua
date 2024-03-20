525f7e240676569ebff94f7422b9fd326d4aa62b
David Andrs <andrsd@gmail.com>
2024-03-20 11:32:31 -0600

    Pass correct handle into `aquad.OptimizeForPolarSymmetry`
    A nil value was really passed into `aquad.OptimizeForPolarSymmetry` since there was a typo in the lua variable name.

--- test/modules/linear_boltzmann_solvers/transport_steady/transport_2d_1_poly.lua
+++ test/modules/linear_boltzmann_solvers/transport_steady/transport_2d_1_poly.lua
@@ -67,7 +67,7 @@ mat.SetProperty(materials[2], ISOTROPIC_MG_SOURCE, FROM_ARRAY, src)
 
 --############################################### Setup Physics
 pquad0 = aquad.CreateProductQuadrature(GAUSS_LEGENDRE_CHEBYSHEV, 2, 1)
-aquad.OptimizeForPolarSymmetry(pquad, 4.0*math.pi)
+aquad.OptimizeForPolarSymmetry(pquad0, 4.0*math.pi)
 
 lbs_block =
 {
