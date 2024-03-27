002d7199aaaf67b3c9c58e10fb24ed201651370c
David Andrs <andrsd@gmail.com>
2024-03-27 12:43:19 -0600

    ExportMultiFieldFunctionToVTK takes vector of field function handles as a 1st argument
    Typically a result of xyz.GetScalarFieldFunctionList is passed into fieldfunc.ExportToVTKMulti
    which is a list of handles.

--- lua/framework/physics/field_function/physics_export_field_func.cc
+++ lua/framework/physics/field_function/physics_export_field_func.cc
@@ -36,58 +36,23 @@ ExportFieldFunctionToVTK(lua_State* L)
 int
 ExportMultiFieldFunctionToVTK(lua_State* L)
 {
-  const std::string fname = "ExportMultiFieldFunctionToVTK";
-  const int num_args = lua_gettop(L);
-  if (num_args != 2)
-    LuaPostArgAmountError(fname, 2, num_args);
+  const std::string fname = "fieldfunc.ExportToVTKMulti";
+  LuaCheckArgs<std::vector<size_t>, std::string>(L, fname);
 
+  auto ff_handles = LuaArg<std::vector<size_t>>(L, 1);
   auto base_name = LuaArg<std::string>(L, 2);
 
-  LuaCheckTableValue(fname, L, 1);
-
-  auto& ff_stack = opensn::field_function_stack;
-
-  const size_t table_size = lua_rawlen(L, 1);
   std::vector<std::shared_ptr<const FieldFunctionGridBased>> ffs;
-  ffs.reserve(table_size);
-  for (int i = 0; i < table_size; ++i)
+  ffs.reserve(ff_handles.size());
+  for (std::size_t i = 0; i < ff_handles.size(); ++i)
   {
-    LuaPush(L, i + 1);
-    lua_gettable(L, 1);
-
-    std::shared_ptr<FieldFunction> ff_base = nullptr;
-    if (lua_isinteger(L, -1))
-    {
-      int ff_handle = lua_tointeger(L, -1);
-      lua_pop(L, 1);
-
-      ff_base = opensn::GetStackItemPtr(ff_stack, ff_handle, fname);
-    }
-    else if (lua_isstring(L, -1))
-    {
-      const std::string ff_name = lua_tostring(L, -1);
-      lua_pop(L, 1);
-
-      for (auto& ff_ptr : ff_stack)
-        if (ff_ptr->TextName() == ff_name)
-        {
-          ff_base = ff_ptr;
-          break;
-        }
-
-      OpenSnInvalidArgumentIf(not ff_base,
-                              "Field function with name \"" + ff_name + "\" could not be found.");
-    }
-    else
-      OpenSnInvalidArgument("The field function specification can only be "
-                            "string names or integer handles.");
-
+    std::shared_ptr<FieldFunction> ff_base =
+      opensn::GetStackItemPtr(opensn::field_function_stack, ff_handles[i], fname);
     auto ff = std::dynamic_pointer_cast<FieldFunctionGridBased>(ff_base);
-
     OpenSnLogicalErrorIf(not ff, "Only grid-based field functions can be exported");
 
     ffs.push_back(ff);
-  } // for i
+  }
 
   FieldFunctionGridBased::ExportMultipleToVTK(base_name, ffs);
 
