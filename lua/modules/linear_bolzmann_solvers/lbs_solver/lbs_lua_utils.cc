7928ac2d4928044dba8a8fa035cc0392eb13c975
David Andrs <andrsd@gmail.com>
2024-05-02 10:30:21 -0600

    Removing unused #include
    Refs #150

--- lua/modules/linear_bolzmann_solvers/lbs_solver/lbs_lua_utils.cc
+++ lua/modules/linear_bolzmann_solvers/lbs_solver/lbs_lua_utils.cc
@@ -1,7 +1,6 @@
 // SPDX-FileCopyrightText: 2024 The OpenSn Authors <https://open-sn.github.io/opensn/>
 // SPDX-License-Identifier: MIT
 
-#include "lbs_common_lua_functions.h"
 #include "lua/framework/lua.h"
 
 using namespace opensn;
