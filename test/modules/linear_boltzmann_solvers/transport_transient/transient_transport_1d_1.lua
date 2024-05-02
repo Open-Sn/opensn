6fd818f828e31c0ffdc97b6ae4b4be01bcb7f75a
Zachary Hardy <zhardy@lanl.gov>
2024-05-02 13:57:12 -0600

    Updated run scripts

--- test/modules/linear_boltzmann_solvers/transport_transient/transient_transport_1d_1.lua
+++ test/modules/linear_boltzmann_solvers/transport_transient/transient_transport_1d_1.lua
@@ -8,25 +8,25 @@ num_procs = 2
 
 
 --############################################### Check num_procs
-if (check_num_procs==nil and number_of_processes ~= num_procs) then
-    log.Log(LOG_0ERROR,"Incorrect amount of processors. " ..
-                       "Expected "..tostring(num_procs)..
-                       ". Pass check_num_procs=false to override if possible.")
+if (check_num_procs == nil and number_of_processes ~= num_procs) then
+    log.Log(LOG_0ERROR, "Incorrect amount of processors. " ..
+        "Expected " .. tostring(num_procs) ..
+        ". Pass check_num_procs=false to override if possible.")
     os.exit(false)
 end
 
 --############################################### Setup mesh
-nodes={}
-N=20
-L=100.0
+nodes = {}
+N = 20
+L = 100.0
 xmin = 0.0
-dx = L/N
-for i=1,(N+1) do
-    k=i-1
-    nodes[i] = xmin + k*dx
+dx = L / N
+for i = 1, (N + 1) do
+    k = i - 1
+    nodes[i] = xmin + k * dx
 end
 
-meshgen1 = mesh.OrthogonalMeshGenerator.Create({ node_sets = {nodes} })
+meshgen1 = mesh.OrthogonalMeshGenerator.Create({ node_sets = { nodes } })
 mesh.MeshGenerator.Execute(meshgen1)
 
 --############################################### Set Material IDs
@@ -44,20 +44,20 @@ mat.AddProperty(materials[1], ISOTROPIC_MG_SOURCE)
 mat.AddProperty(materials[2], ISOTROPIC_MG_SOURCE)
 
 -- Define microscopic cross sections
-xss = xs.Create()
+micro_xs = xs.Create()
 xs_file = "tests/transport_transient/subcritical_1g.xs"
-xs.Set(xss, OPENSN_XSFILE, xs_file)
+xs.Set(micro_xs, OPENSN_XSFILE, xs_file)
 
 -- Define macroscopic cross sections
---xss = xs.MakeCombined({{xss, 0.00264086}}) -- just sub-critical
-xss = xs.MakeCombined({{xss, 0.0424459}}) -- just sub-critical
+--macro_xs = xs.MakeScaled(micro_xs, 0.00264086) -- just sub-critical
+macro_xs = xs.MakeCombined(micro_xs, 0.0424459) -- just sub-critical
 
 num_groups = 1
-mat.SetProperty(materials[1], TRANSPORT_XSECTIONS, EXISTING, xss)
-mat.SetProperty(materials[2], TRANSPORT_XSECTIONS, EXISTING, xss)
+mat.SetProperty(materials[1], TRANSPORT_XSECTIONS, EXISTING, macro_xs)
+mat.SetProperty(materials[2], TRANSPORT_XSECTIONS, EXISTING, macro_xs)
 
-src={}
-for g=1,num_groups do
+src = {}
+for g = 1, num_groups do
     src[g] = 0.0
 end
 --src[1] = 1.0
@@ -69,7 +69,7 @@ phys1 = LBSCreateTransientSolver()
 
 --========== Groups
 grp = {}
-for g=1,num_groups do
+for g = 1, num_groups do
     grp[g] = LBSCreateGroup(phys1)
 end
 
@@ -79,14 +79,14 @@ pquad = aquad.CreateProductQuadrature(GAUSS_LEGENDRE, 16)
 --========== Groupset def
 gs0 = LBSCreateGroupset(phys1)
 cur_gs = gs0
-LBSGroupsetAddGroups(phys1,cur_gs,0,num_groups-1)
-LBSGroupsetSetQuadrature(phys1,cur_gs,pquad)
-LBSGroupsetSetAngleAggDiv(phys1,cur_gs,1)
-LBSGroupsetSetGroupSubsets(phys1,cur_gs,8)
-LBSGroupsetSetIterativeMethod(phys1,cur_gs,KRYLOV_GMRES)
-LBSGroupsetSetResidualTolerance(phys1,cur_gs,1.0e-6)
-LBSGroupsetSetMaxIterations(phys1,cur_gs,1000)
-LBSGroupsetSetGMRESRestartIntvl(phys1,cur_gs,100)
+LBSGroupsetAddGroups(phys1, cur_gs, 0, num_groups - 1)
+LBSGroupsetSetQuadrature(phys1, cur_gs, pquad)
+LBSGroupsetSetAngleAggDiv(phys1, cur_gs, 1)
+LBSGroupsetSetGroupSubsets(phys1, cur_gs, 8)
+LBSGroupsetSetIterativeMethod(phys1, cur_gs, KRYLOV_GMRES)
+LBSGroupsetSetResidualTolerance(phys1, cur_gs, 1.0e-6)
+LBSGroupsetSetMaxIterations(phys1, cur_gs, 1000)
+LBSGroupsetSetGMRESRestartIntvl(phys1, cur_gs, 100)
 --LBSGroupsetSetWGDSA(phys1,cur_gs,30,1.0e-4,false," ")
 --LBSGroupsetSetTGDSA(phys1,cur_gs,30,1.0e-4,false," ")
 
@@ -99,8 +99,8 @@ LBSGroupsetSetGMRESRestartIntvl(phys1,cur_gs,100)
 --bsrc[1] = 1.0/2
 ----LBSSetProperty(phys1,BOUNDARY_CONDITION,ZMIN,LBSBoundaryTypes.INCIDENT_ISOTROPIC,bsrc);
 --
-LBSSetProperty(phys1,DISCRETIZATION_METHOD,PWLD)
-LBSSetProperty(phys1,SCATTERING_ORDER,1)
+LBSSetProperty(phys1, DISCRETIZATION_METHOD, PWLD)
+LBSSetProperty(phys1, SCATTERING_ORDER, 1)
 
 LBKESSetProperty(phys1, "MAX_ITERATIONS", 100)
 LBKESSetProperty(phys1, "TOLERANCE", 1.0e-8)
@@ -134,15 +134,15 @@ phys1name = solver.GetName(phys1);
 
 time = 0.0
 time_stop = 20.0
-k=0
+k = 0
 while (time < time_stop) do
     k = k + 1
     solver.Step(phys1)
-    FRf = lbs.ComputeFissionRate(phys1,"NEW")
-    FRi = lbs.ComputeFissionRate(phys1,"OLD")
+    FRf = lbs.ComputeFissionRate(phys1, "NEW")
+    FRi = lbs.ComputeFissionRate(phys1, "OLD")
     dt = LBTSGetProperty(phys1, "TIMESTEP")
     time = LBTSGetProperty(phys1, "TIME")
-    period = dt/math.log(FRf/FRi)
+    period = dt / math.log(FRf / FRi)
     log.Log(LOG_0, string.format("%s %4d time=%10.3g dt=%10.4g period=%10.3g FR=%10.3e",
-            phys1name,k,time,dt,period,FRf))
+                                 phys1name, k, time, dt, period, FRf))
 end
