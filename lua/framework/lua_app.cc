3c1c73dc9d66acd0f006023fd3ad0726e5704e19
Hawkins <dhawkins@NUEN-FHKGJ9324H.local>
2024-06-19 11:15:02 -0500

    Updating processing order of cli options so that verbosity is processed before other options.

--- lua/framework/lua_app.cc
+++ lua/framework/lua_app.cc
@@ -113,6 +113,7 @@ LuaApp::ProcessArguments(int argc, char** argv)
 
     auto result = options.parse(argc, argv);
 
+    // Note that the order of evaluation of the command-line options is important!
     if (result.count("help"))
     {
       if (opensn::mpi_comm.rank() == 0)
@@ -127,6 +128,12 @@ LuaApp::ProcessArguments(int argc, char** argv)
       return 1;
     }
 
+    if (result.count("verbose"))
+    {
+      int verbosity = result["verbose"].as<int>();
+      opensn::log.SetVerbosity(verbosity);
+    }
+
     if (result.count("allow-petsc-error-handler"))
       allow_petsc_error_handler_ = true;
 
@@ -138,12 +145,6 @@ LuaApp::ProcessArguments(int argc, char** argv)
       ObjectFactory::GetInstance().DumpRegister();
       console.DumpRegister();
       return 1;
-    }
-
-    if (result.count("verbose"))
-    {
-      int verbosity = result["verbose"].as<int>();
-      opensn::log.SetVerbosity(verbosity);
     }
 
     if (result.count("caliper"))
