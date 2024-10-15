f06a610f102ef76ec2b15baa9154a12b2f02a793
David Andrs <andrsd@gmail.com>
2024-10-15 08:10:46 -0600

    Enforcing code standard in VertexHandler

--- framework/mesh/mesh_continuum/mesh_continuum_vertex_handler.h
+++ framework/mesh/mesh_continuum/mesh_continuum_vertex_handler.h
@@ -15,33 +15,33 @@ class VertexHandler
   using GlobalIDMap = std::map<uint64_t, Vector3>;
 
 private:
-  std::map<uint64_t, Vector3> m_global_id_vertex_map;
+  std::map<uint64_t, Vector3> m_global_id_vertex_map_;
 
 public:
   // Iterators
-  GlobalIDMap::iterator begin() { return m_global_id_vertex_map.begin(); }
-  GlobalIDMap::iterator end() { return m_global_id_vertex_map.end(); }
+  GlobalIDMap::iterator begin() { return m_global_id_vertex_map_.begin(); }
+  GlobalIDMap::iterator end() { return m_global_id_vertex_map_.end(); }
 
-  GlobalIDMap::const_iterator begin() const { return m_global_id_vertex_map.begin(); }
-  GlobalIDMap::const_iterator end() const { return m_global_id_vertex_map.end(); }
+  GlobalIDMap::const_iterator begin() const { return m_global_id_vertex_map_.begin(); }
+  GlobalIDMap::const_iterator end() const { return m_global_id_vertex_map_.end(); }
 
   // Accessors
-  Vector3& operator[](const uint64_t global_id) { return m_global_id_vertex_map.at(global_id); }
+  Vector3& operator[](const uint64_t global_id) { return m_global_id_vertex_map_.at(global_id); }
 
   const Vector3& operator[](const uint64_t global_id) const
   {
-    return m_global_id_vertex_map.at(global_id);
+    return m_global_id_vertex_map_.at(global_id);
   }
 
   // Utilities
   void Insert(const uint64_t global_id, const Vector3& vec)
   {
-    m_global_id_vertex_map.insert(std::make_pair(global_id, vec));
+    m_global_id_vertex_map_.insert(std::make_pair(global_id, vec));
   }
 
-  size_t NumLocallyStored() const { return m_global_id_vertex_map.size(); }
+  size_t NumLocallyStored() const { return m_global_id_vertex_map_.size(); }
 
-  void Clear() { m_global_id_vertex_map.clear(); }
+  void Clear() { m_global_id_vertex_map_.clear(); }
 };
 
 } // namespace opensn
