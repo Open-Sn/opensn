0c967c51573ce41493bbbf7b54329485b8629907
danny.nguyen <danny.nguyen@nuen-class02.engr.tamu.edu>
2025-02-25 12:16:21 -0600

    Implement the Python interface for the first LBS example

---
+++ pyopensn/src/main.cpp
@@ -0,0 +1,37 @@
+/*
+ * Created on Sat, February 22
+ *
+ * Copyright (c) 2025 The OpenSn Authors <https://open-sn.github.io/opensn/>
+ */
+#include "pyapi.hpp"
+
+#include "petsc.h"
+#include "framework/runtime.h"
+
+#include "config.h"  // PROJECT_VERSION
+
+PYBIND11_MODULE(pyopensn, pyopensn) {
+    using namespace opensn;
+    // metadata
+    pyopensn.doc() = "Python interface for OpenSn.";
+    pyopensn.attr("__version__") = PROJECT_VERSION;
+    // environment
+    if (PyEnv::p_default_env == nullptr) {
+        PyEnv::p_default_env = new PyEnv();
+    }
+    ::Py_AtExit(
+        [](void) noexcept {
+            delete PyEnv::p_default_env;
+            PyEnv::p_default_env = nullptr;
+        }
+    );
+    // wrap libraries
+    py_aquad(pyopensn);
+    py_ffunc(pyopensn);
+    py_logvol(pyopensn);
+    py_mat(pyopensn);
+    py_mesh(pyopensn);
+    py_solver(pyopensn);
+    py_source(pyopensn);
+    py_xs(pyopensn);
+}
