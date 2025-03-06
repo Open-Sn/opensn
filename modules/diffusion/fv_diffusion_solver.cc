e2a0eb6192c5e076d2e6b6072f05bafdf4e8e508
David Andrs <andrsd@gmail.com>
2025-03-06 09:05:51 -0700

    Renaming `imat` and `iblk` to `block_id`

--- modules/diffusion/fv_diffusion_solver.cc
+++ modules/diffusion/fv_diffusion_solver.cc
@@ -272,11 +272,11 @@ FVDiffusionSolver::Execute()
     const auto volume_P = cell_P.volume;
     const auto& x_cc_P = cell_P.centroid;
 
-    const auto imat = cell_P.block_id;
+    const auto block_id = cell_P.block_id;
 
-    const double sigma_a = sigma_a_function_->Evaluate(imat, x_cc_P);
-    const double q_ext = q_ext_function_->Evaluate(imat, x_cc_P);
-    const double D_P = d_coef_function_->Evaluate(imat, x_cc_P);
+    const double sigma_a = sigma_a_function_->Evaluate(block_id, x_cc_P);
+    const double q_ext = q_ext_function_->Evaluate(block_id, x_cc_P);
+    const double D_P = d_coef_function_->Evaluate(block_id, x_cc_P);
 
     const int64_t imap = sdm.MapDOF(cell_P, 0);
     MatSetValue(A_, imap, imap, sigma_a * volume_P, ADD_VALUES);
