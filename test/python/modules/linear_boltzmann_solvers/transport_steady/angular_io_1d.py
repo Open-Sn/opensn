61e06adbd90b46bd85d8b31c8cdb464652575d40
Jean Ragusa <jean.ragusa@tamu.edu>
2025-03-27 17:10:00 -0500

    Update angular_io_1d.py

--- test/python/modules/linear_boltzmann_solvers/transport_steady/angular_io_1d.py
+++ test/python/modules/linear_boltzmann_solvers/transport_steady/angular_io_1d.py
@@ -5,66 +5,59 @@
 
 import os
 import sys
-import math
 
 if "opensn_console" not in globals():
     from mpi4py import MPI
     size = MPI.COMM_WORLD.size
     rank = MPI.COMM_WORLD.rank
     sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../../")))
-    from pyopensn.mesh import OrthogonalMeshGenerator, KBAGraphPartitioner
+    from pyopensn.mesh import OrthogonalMeshGenerator
     from pyopensn.xs import MultiGroupXS
     from pyopensn.source import VolumetricSource
     from pyopensn.aquad import GLProductQuadrature1DSlab
-    from pyopensn.solver import DiscreteOrdinatesSolver, SteadyStateSolver
-    from pyopensn.fieldfunc import FieldFunctionGridBased
-    from pyopensn.fieldfunc import FieldFunctionInterpolationLine, FieldFunctionInterpolationVolume
-    from pyopensn.settings import EnableCaliper
-    from pyopensn.math import Vector3
+    from pyopensn.solver import SteadyStateSolver
     from pyopensn.logvol import RPPLogicalVolume
 
 if __name__ == "__main__":
 
+    widths = [ 2., 1., 2., 1., 2. ]
+    nrefs = [ 200, 200, 200, 200, 200 ]
 
-    widths = { 2., 1., 2., 1., 2. }
-    nrefs = { 200, 200, 200, 200, 200 }
+    Nmat = len(widths)
 
-    Nmat = #widths
-
-    nodes = []
-    counter = 1
-    nodes[counter] = 0.
-    for imat in range(1, Nmat+1):
+    nodes = [0.]
+    for imat in range(Nmat):
       dx = widths[imat] / nrefs[imat]
-      for i in range(1, nrefs[imat]+1):
-        counter = counter + 1
-        nodes[counter] = nodes[counter - 1] + dx
+      for i in range(nrefs[imat]):
+        nodes.append( nodes[-1] + dx )
 
     meshgen = OrthogonalMeshGenerator(node_sets = [nodes])
-    grid = meshgen.Execute(meshgen)
+    grid = meshgen.Execute()
 
-    # Set Material IDs
+    # Set block IDs
     z_min = 0.0
     z_max = widths[1]
-    for imat in range(1, Nmat+1):
+    for imat in range(Nmat):
       z_max = z_min + widths[imat]
-      log.Log(LOG_0, "imat=" + imat + ", zmin=" + z_min + ", zmax=" + z_max)
+      print("imat=",imat,", zmin=",z_min,", zmax=",z_max)
       lv = RPPLogicalVolume( infx = True, infy = True, zmin = z_min, zmax = z_max )
-      grid.SetBlockIDFromLogicalVolume(lv, imat - 1, True)
+      grid.SetBlockIDFromLogical(lv, imat, True)
       z_min = z_max
 
     # Add cross sections to materials
-    total = { 50., 5., 0., 1., 1. }
-    c = { 0., 0., 0., 0.9, 0.9 }
-    xs_map = []
-    for imat in range(1, Nmat+1):
+    total = [ 50., 5., 0., 1., 1. ]
+    c = [ 0., 0., 0., 0.9, 0.9 ]
+    xs_map = len(total)*[None]
+    for imat in range(Nmat):
+      xs_ = MultiGroupXS()
+      xs_.CreateSimpleOneGroup(total[imat], c[imat])
       xs_map[imat] = {
-        "block_ids": [ imat - 1 ], "xs": xs.CreateSimpleOneGroup(total[imat], c[imat]),
+        "block_ids": [ imat ], "xs": xs_,
       }
 
     # Create sources in 1st and 4th materials
-    src0 = VolumetricSource( block_ids = [ 0 ], group_strength = { 50. } )
-    src1 = VolumetricSource( block_ids = [ 3 ], group_strength = { 1. } )
+    src0 = VolumetricSource( block_ids = [ 0 ], group_strength = [ 50. ] )
+    src1 = VolumetricSource( block_ids = [ 3 ], group_strength = [  1. ] )
 
     # Angular Quadrature
     gl_quad = GLProductQuadrature1DSlab(128)
@@ -74,26 +67,29 @@ if __name__ == "__main__":
     phys = DiscreteOrdinatesSolver(
       mesh = grid,
       num_groups = num_groups,
-      groupsets = {
+      groupsets = [
         {
-          groups_from_to = { 0, num_groups - 1 },
-          angular_quadrature = gl_quad,
-          inner_linear_method = "petsc_gmres",
-          l_abs_tol = 1.0e-9,
-          l_max_its = 300,
-          gmres_restart_interval = 30,
+          "groups_from_to": (0, num_groups - 1),
+          "angular_quadrature": gl_quad,
+          "inner_linear_method": "petsc_gmres",
+          "l_abs_tol": 1.0e-9,
+          "l_max_its": 300,
+          "gmres_restart_interval": 30,
         },
-      },
+      ],
       xs_map = xs_map,
       options = {
         "scattering_order": 0,
         "spatial_discretization": "pwld",
-        "boundary_conditions": [ { "name": "zmin", "type": "vacuum" }, { "name": "zmax", "type": "vacuum" } ],
-        "save_angular_flux": True,
+        "boundary_conditions": [
+            { "name": "zmin", "type": "vacuum" },
+            { "name": "zmax", "type": "vacuum" }
+        ],
         "volumetric_sources": [ src0, src1 ],
-      },
-    ]
-
+        "save_angular_flux": True,
+      }
+    )
+    
 
     # Initialize and execute solver
     ss_solver = SteadyStateSolver( lbs_solver = phys )
@@ -121,8 +117,8 @@ if __name__ == "__main__":
     leakage_right_diff = leakage_right_1 - leakage_right_2
 
     if rank == 0:
-    print(f"Leakage-Diff1={leakage_left_diff:.5e}")
+      print(f"Leakage-Diff1={leakage_left_diff:.5e}")
     if rank == 0:
-    print(f"Leakage-Diff2={leakage_right_diff:.5e}")
+      print(f"Leakage-Diff2={leakage_right_diff:.5e}")
 
     os.system("rm angular_io0.h5")
