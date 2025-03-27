0f371af6c6f431b39e661c54a1ac0aaea10a03b1
Jean Ragusa <jean.ragusa@tamu.edu>
2025-03-26 22:57:32 -0500

    Update reed_balance.py

--- test/python/modules/linear_boltzmann_solvers/transport_steady/reed_balance.py
+++ test/python/modules/linear_boltzmann_solvers/transport_steady/reed_balance.py
@@ -72,15 +72,15 @@ if __name__ == "__main__":
       num_groups = num_groups,
       groupsets = [
         {
-          groups_from_to = { 0, num_groups - 1 },
-          angular_quadrature = gl_quad,
-          inner_linear_method = "petsc_gmres",
-          l_abs_tol = 1.0e-9,
-          l_max_its = 300,
-          gmres_restart_interval = 30,
+          "groups_from_to": { 0, num_groups - 1 },
+          "angular_quadrature": gl_quad,
+          "inner_linear_method": "petsc_gmres",
+          "l_abs_tol": 1.0e-9,
+          "l_max_its": 300,
+          "gmres_restart_interval": 30,
         },
       },
-      xs_map = xs_map,
+      "xs_map": xs_map,
       options = {
         "scattering_order": 0,
         "spatial_discretization": "pwld",
