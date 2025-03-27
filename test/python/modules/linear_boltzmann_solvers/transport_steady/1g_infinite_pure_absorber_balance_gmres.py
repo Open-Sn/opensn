164f0efb371c2262c56e8ecda6f4c36ea3f6ac7b
ragusa <jean.ragusa@tamu.edu>
2025-03-26 22:39:39 -0500

    mac

--- test/python/modules/linear_boltzmann_solvers/transport_steady/1g_infinite_pure_absorber_balance_gmres.py
+++ test/python/modules/linear_boltzmann_solvers/transport_steady/1g_infinite_pure_absorber_balance_gmres.py
@@ -12,13 +12,11 @@ if "opensn_console" not in globals():
     size = MPI.COMM_WORLD.size
     rank = MPI.COMM_WORLD.rank
     sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../../")))
-    from pyopensn.mesh import OrthogonalMeshGenerator, KBAGraphPartitioner
+    from pyopensn.mesh import OrthogonalMeshGenerator
     from pyopensn.xs import MultiGroupXS
     from pyopensn.source import VolumetricSource
     from pyopensn.aquad import GLCProductQuadrature3DXYZ
     from pyopensn.solver import DiscreteOrdinatesSolver, SteadyStateSolver
-    from pyopensn.fieldfunc import FieldFunctionGridBased
-    from pyopensn.settings import EnableCaliper
 
 if __name__ == "__main__":
 
