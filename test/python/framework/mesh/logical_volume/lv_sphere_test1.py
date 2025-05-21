9e8b322ad34fc46ba1567d13cd1289daf4e3ab33
ragusa <jean.ragusa@tamu.edu>
2025-05-21 14:40:20 -0500

    tutorials for logvol

--- test/python/framework/mesh/logical_volume/lv_sphere_test1.py
+++ test/python/framework/mesh/logical_volume/lv_sphere_test1.py
@@ -3,23 +3,14 @@
 
 import os
 import sys
-import math
 
 if "opensn_console" not in globals():
     from mpi4py import MPI
     size = MPI.COMM_WORLD.size
     rank = MPI.COMM_WORLD.rank
     sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../../")))
-    from pyopensn.mesh import OrthogonalMeshGenerator, KBAGraphPartitioner
-    from pyopensn.xs import MultiGroupXS
-    from pyopensn.source import VolumetricSource
-    from pyopensn.aquad import GLProductQuadrature1DSlab
-    from pyopensn.solver import DiscreteOrdinatesProblem, SteadyStateSolver
-    from pyopensn.fieldfunc import FieldFunctionGridBased
-    from pyopensn.fieldfunc import FieldFunctionInterpolationLine, FieldFunctionInterpolationVolume
-    from pyopensn.settings import EnableCaliper
     from pyopensn.math import Vector3
-    from pyopensn.logvol import RPPLogicalVolume
+    from pyopensn.logvol import SphereLogicalVolume
 
 if __name__ == "__main__":
 
