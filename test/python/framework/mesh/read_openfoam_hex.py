b6be195980cdd0ef5f07cdfe08132ffc4d82c942
kitamuraa24 <kitamuraa@tamu.edu>
2025-08-20 13:08:45 -0500

    Implemented OpenFOAM polyMesh reader with regression test for Hex, Tets, Wedge, and Pyr elements.

---
+++ test/python/framework/mesh/read_openfoam_hex.py
@@ -0,0 +1,18 @@
+import sys
+import os
+
+if "opensn_console" not in globals():
+    from mpi4py import MPI
+    size = MPI.COMM_WORLD.size
+    rank = MPI.COMM_WORLD.rank
+    # Append parent directory to locate the pyopensn modules
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../../")))
+    from pyopensn.mesh import FromFileMeshGenerator
+
+if __name__ == "__main__":
+
+    # Test Hex
+    meshgen = FromFileMeshGenerator(
+        filename="openfoam_meshes/hex"
+    )
+    grid = meshgen.Execute()
