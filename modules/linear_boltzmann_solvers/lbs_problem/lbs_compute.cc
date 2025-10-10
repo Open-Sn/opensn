815689d87fc14e51af39112398fa0298a9218fd6
David Andrs <andrsd@gmail.com>
2025-10-10 12:05:59 -0600

    Correcting `table_size` type
    `MPI_Allreduce` can send only `int` counts.

--- modules/linear_boltzmann_solvers/lbs_problem/lbs_compute.cc
+++ modules/linear_boltzmann_solvers/lbs_problem/lbs_compute.cc
@@ -292,7 +292,7 @@ ComputeBalance(DiscreteOrdinatesProblem& do_problem)
   double local_gain = local_production + local_in_flow;
   std::vector<double> local_balance_table = {
     local_absorption, local_production, local_in_flow, local_out_flow, local_balance, local_gain};
-  size_t table_size = local_balance_table.size();
+  auto table_size = static_cast<int>(local_balance_table.size());
 
   // Compute global balance
   std::vector<double> global_balance_table(table_size, 0.0);
