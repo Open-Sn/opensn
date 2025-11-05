36f5071aad5946d71e443d17b4b13200c68fc69f
Daryl Hawkins <daryl@thelostpacket.com>
2025-11-04 21:38:18 -0800

    Updating CMake dependencies scipt for better handling of HDF5 and improved output/logging.

--- tools/dependencies/env_script.cmake
+++ tools/dependencies/env_script.cmake
@@ -3,5 +3,19 @@ set(ENV_SCRIPT "${OPENSN_ENV_DIR}/set_opensn_env.sh")
 file(MAKE_DIRECTORY "${OPENSN_ENV_DIR}")
 file(WRITE "${ENV_SCRIPT}" "export CMAKE_PREFIX_PATH=\"${CMAKE_INSTALL_PREFIX}\"\${CMAKE_PREFIX_PATH:+:\${CMAKE_PREFIX_PATH}}\n")
 file(CHMOD "${ENV_SCRIPT}" FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE)
-message(STATUS "OpenSn dependency install complete.")
-message(STATUS "To update your CMAKE_PREFIX_PATH environment variable, execute the command: source ${ENV_SCRIPT}")
+
+message(STATUS "
+*******************************************************************************
+*******************************************************************************
+
+ OpenSn dependency install complete.
+
+ To update your CMAKE_PREFIX_PATH for the current shell, run:
+
+   source ${ENV_SCRIPT}
+
+ (This adjusts only the current shell session. Add the line above to your
+  shell init file - e.g., ~/.bashrc or ~/.zshrc - to make it permanent.)
+
+*******************************************************************************
+*******************************************************************************")
