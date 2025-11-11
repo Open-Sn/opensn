a8c6763f3fdb8136fa17b58192e615400536caec
David Andrs <andrsd@gmail.com>
2025-11-11 14:06:57 -0700

    Promoting mpicpp-lite into a full-blooded dependency
    - mpicpp-lite is now treated as a regular dependency
    - OpenSn behaves more as a library, i.e. does not come with
      bundled dependecies
    - this helps applications depending on OpenSn not to re-package
      mpicpp-lite and just use ti via `find_package`

--- external/mpicpp-lite/impl/Enums.h
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2023 David Andrs <andrsd@gmail.com>
-// SPDX-License-Identifier: MIT
-
-#pragma once
-
-#include "mpi.h"
-
-enum { UNDEFINED = MPI_UNDEFINED, ANY_SOURCE = MPI_ANY_SOURCE, ANY_TAG = MPI_ANY_TAG };
