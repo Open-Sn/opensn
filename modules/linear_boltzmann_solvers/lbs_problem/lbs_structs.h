bcfd1e88c6a96339f4e4d008d8967966979a9e0d
David Andrs <andrsd@gmail.com>
2025-12-18 13:20:37 -0700

    Number of iterations in `AGSLinearSolver` is unsigned int

--- modules/linear_boltzmann_solvers/lbs_problem/lbs_structs.h
+++ modules/linear_boltzmann_solvers/lbs_problem/lbs_structs.h
@@ -60,7 +60,7 @@ struct LBSOptions
   bool verbose_ags_iterations = true;
   bool verbose_outer_iterations = true;
   bool ags_pointwise_convergence = false;
-  int max_ags_iterations = 100;
+  unsigned int max_ags_iterations = 100;
   double ags_tolerance = 1.0e-6;
 
   bool power_field_function_on = false;
