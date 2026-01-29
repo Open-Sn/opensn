c70cdfd0d7ae701d1d5acef1d83175d188b0c00e
David Andrs <andrsd@gmail.com>
2026-01-29 13:15:41 -0700

    Basic groundwork for volume post-processor

--- python/lib/py_app.cc
+++ python/lib/py_app.cc
@@ -67,6 +67,7 @@ PyApp::PyApp(const mpi::Communicator& comm)
   Console::BindModule(WrapNLKEigen);
   Console::BindModule(WrapPIteration);
   Console::BindModule(WrapDiscreteOrdinatesKEigenAcceleration);
+  Console::BindModule(WrapPostprocessors);
 }
 
 int
