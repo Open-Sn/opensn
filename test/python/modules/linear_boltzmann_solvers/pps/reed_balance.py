953b0fa52c86ff873d5feb5d01788114a7570039
David Andrs <andrsd@gmail.com>
2026-02-19 10:28:31 -0700

    Adding basic tests for 1G problem

---
+++ test/python/modules/linear_boltzmann_solvers/pps/reed_balance.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Standard Reed 1D 1-group problem
+
+import os
+import sys
+
+if "opensn_console" not in globals():
+    from mpi4py import MPI
+    size = MPI.COMM_WORLD.size
+    rank = MPI.COMM_WORLD.rank
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../../../")))
+    from pyopensn.mesh import OrthogonalMeshGenerator
+    from pyopensn.xs import MultiGroupXS
+    from pyopensn.source import VolumetricSource
+    from pyopensn.aquad import GLProductQuadrature1DSlab
+    from pyopensn.solver import DiscreteOrdinatesProblem, SteadyStateSourceSolver
+    from pyopensn.logvol import RPPLogicalVolume
+    from pyopensn.post import VolumePostprocessor
+
+if __name__ == "__main__":
+
+    # Create Mesh
+    widths = [2., 1., 2., 1., 2.]
+    nrefs = [200, 200, 200, 200, 200]
+    Nmat = len(widths)
+    nodes = [0.]
+    for imat in range(Nmat):
+        dx = widths[imat] / nrefs[imat]
+        for i in range(nrefs[imat]):
+            nodes.append(nodes[-1] + dx)
+    meshgen = OrthogonalMeshGenerator(node_sets=[nodes])
+    grid = meshgen.Execute()
+
+    # Set block IDs
+    z_min = 0.0
+    z_max = widths[1]
+    for imat in range(Nmat):
+        z_max = z_min + widths[imat]
+        if rank == 0:
+            print("imat=", imat, ", zmin=", z_min, ", zmax=", z_max)
+        lv = RPPLogicalVolume(infx=True, infy=True, zmin=z_min, zmax=z_max)
+        grid.SetBlockIDFromLogicalVolume(lv, imat, True)
+        z_min = z_max
+
+    # Add cross sections to materials
+    total = [50., 5., 0., 1., 1.]
+    c = [0., 0., 0., 0.9, 0.9]
+    xs_map = len(total) * [None]
+    for imat in range(Nmat):
+        xs_ = MultiGroupXS()
+        xs_.CreateSimpleOneGroup(total[imat], c[imat])
+        xs_map[imat] = {
+            "block_ids": [imat], "xs": xs_,
+        }
+
+    # Create sources in 1st and 4th materials
+    src0 = VolumetricSource(block_ids=[0], group_strength=[50.])
+    src1 = VolumetricSource(block_ids=[3], group_strength=[1.])
+
+    # Angular Quadrature
+    gl_quad = GLProductQuadrature1DSlab(n_polar=128, scattering_order=0)
+
+    # LBS block option
+    num_groups = 1
+    phys = DiscreteOrdinatesProblem(
+        mesh=grid,
+        num_groups=num_groups,
+        groupsets=[
+            {
+                "groups_from_to": (0, num_groups - 1),
+                "angular_quadrature": gl_quad,
+                "inner_linear_method": "petsc_gmres",
+                "l_abs_tol": 1.0e-9,
+                "l_max_its": 300,
+                "gmres_restart_interval": 30,
+            },
+        ],
+        xs_map=xs_map,
+        volumetric_sources=[src0, src1],
+        boundary_conditions=[
+            {"name": "zmin", "type": "vacuum"},
+            {"name": "zmax", "type": "vacuum"}
+        ],
+    )
+
+    # Initialize and execute solver
+    ss_solver = SteadyStateSourceSolver(problem=phys)
+    ss_solver.Initialize()
+    ss_solver.Execute()
+
+    # PPS over whole domain
+    pps_whole_min = VolumePostprocessor(problem=phys, value_type="min")
+    pps_whole_min.Execute()
+
+    pps_whole_max = VolumePostprocessor(problem=phys, value_type="max")
+    pps_whole_max.Execute()
+
+    pps_whole_avg = VolumePostprocessor(problem=phys, value_type="avg")
+    pps_whole_avg.Execute()
+
+    pps_whole_int = VolumePostprocessor(problem=phys, value_type="integral")
+    pps_whole_int.Execute()
+
+    if rank ==0:
+        print(f"whole: min = {pps_whole_min.GetValue()[0][0]}")
+        print(f"whole: max = {pps_whole_max.GetValue()[0][0]}")
+        print(f"whole: avg = {pps_whole_avg.GetValue()[0][0]}")
+        print(f"whole: int = {pps_whole_int.GetValue()[0][0]}")
