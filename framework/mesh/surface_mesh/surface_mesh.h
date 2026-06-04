6672e3708a9ea17021f829159a62ea59cfc32aba
David Andrs <andrsd@gmail.com>
2026-06-04 08:37:40 -0600

    Moving surface mesh data-structures into `SurfaceMesh` as nested classes

--- framework/mesh/surface_mesh/surface_mesh.h
+++ framework/mesh/surface_mesh/surface_mesh.h
@@ -10,10 +10,6 @@
 namespace opensn
 {
 
-struct Face;
-struct PolyFace;
-struct Edge;
-
 /**
  * Generic surface mesh class.
  * This class facilitates many functions within the mesh environment including logically determining
@@ -22,6 +18,69 @@ struct Edge;
 class SurfaceMesh
 {
 public:
+  /// Structure containing edge properties
+  struct Edge
+  {
+    /// Indices of the vertices
+    std::array<int, 2> v_index{-1, -1};
+    /// Indices of faces adjoining it
+    std::array<int, 4> f_index{-1, -1, -1, -1};
+    /// Vector vertices
+    std::array<Vector3, 2> vertices{};
+  };
+
+  /// Data structure for a triangular face.
+  struct Face
+  {
+    /// vertex indices
+    std::array<int, 3> v_index{-1, -1, -1};
+    /// normal indices
+    std::array<int, 3> n_index{-1, -1, -1};
+    /// vertex texture indices
+    std::array<int, 3> vt_index{-1, -1, -1};
+    /// edge indices
+    std::array<std::array<int, 4>, 3> e_index{
+      {{-1, -1, -1, -1}, {-1, -1, -1, -1}, {-1, -1, -1, -1}}};
+
+    Vector3 geometric_normal;
+    Vector3 assigned_normal;
+    Vector3 face_centroid;
+
+    bool invalidated{false};
+  };
+
+  /**
+   * Data structure for a polygon face.
+   *
+   * edges
+   * An array of 4 integers.
+   * [0] = Vertex index of edge start.
+   * [1] = Vertex index of edge end.
+   * [2] = Index of the face adjoining this edge (not the current face).
+   *       -1 if not connected to anything,-1*boundary_index if connected
+   *       to a boundary.
+   * [3] = Edge number of adjoining face. -1 if not connected
+   *       to anything. 0 if a boundary.
+   *
+   * face_indices
+   *  [0] = Index of the adjoining cell. -1 if not connected to anything.
+   *        -1*boundary_index if connected to a boundary.
+   *  [1] = Face number of adjoining cell. -1 if not connected
+   *       to anything. 0 if a boundary.
+   *  [2] = Partition ID of adjacent cell.
+   */
+  struct PolyFace
+  {
+    std::vector<int> v_indices;
+    std::vector<std::vector<int>> edges;
+    std::array<int, 3> face_indices{{-1, -1, -1}};
+
+    Vector3 geometric_normal;
+    Vector3 face_centroid;
+
+    bool invalidated{false};
+  };
+
   explicit SurfaceMesh(const InputParameters& params);
 
   const std::vector<Vector3>& GetVertices() const { return vertices_; }
