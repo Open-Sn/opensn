d4978c6c76ac4f62029e27144e88b3c5bf7336aa
rodriguez-jay <j2jason32@tamu.edu>
2026-08-20 16:32:34 -0500

    Add surface angular flux response support

--- modules/linear_boltzmann_solvers/discrete_ordinates_problem/io/discrete_ordinates_problem_io.h
+++ modules/linear_boltzmann_solvers/discrete_ordinates_problem/io/discrete_ordinates_problem_io.h
@@ -6,8 +6,11 @@
 #include "hdf5.h"
 #include <cstdint>
 #include <functional>
+#include <map>
 #include <optional>
 #include <string>
+#include <tuple>
+#include <utility>
 #include <vector>
 
 namespace opensn
@@ -62,6 +65,70 @@ public:
     const std::string& file_base,
     std::optional<std::reference_wrapper<std::vector<std::vector<double>>>> opt_dest =
       std::nullopt);
+
+  /**
+   * Surface Angular flux
+   */
+  using QuantizedCoordinate = std::tuple<int64_t, int64_t, int64_t>;
+  using FaceMap = std::map<QuantizedCoordinate, uint64_t>;
+
+  struct SurfaceMap
+  {
+    std::vector<uint64_t> cell_ids;
+    std::vector<uint64_t> num_face_nodes;
+    std::map<QuantizedCoordinate, uint64_t> cell_map;
+    std::vector<uint64_t> cell_stride;
+    std::vector<FaceMap> faces;
+    std::vector<double> nodes_x;
+    std::vector<double> nodes_y;
+    std::vector<double> nodes_z;
+  };
+
+  struct SurfaceData
+  {
+    std::vector<double> omega;
+    std::vector<double> mu;
+    std::vector<double> wt_d;
+    std::vector<double> mass_matrix;
+    std::vector<double> fe_shape;
+    std::vector<double> psi;
+    std::vector<uint64_t> node_index;
+    std::vector<uint64_t> dir_index;
+  };
+
+  struct SurfaceAngularFlux
+  {
+    int groupset_id = 0;
+    std::string surface_name;
+    SurfaceMap mapping;
+    SurfaceData data;
+  };
+
+  /**
+   * Write surface angular flux vector(s) to a file.
+   *
+   * \param do_problem Discrete ordinates problem
+   * \param file_base File name base
+   * \param bndry_surfs Boundary surface names
+   * \param int_surfs Internal surface definitions
+   */
+  static void WriteSurfaceAngularFluxes(
+    DiscreteOrdinatesProblem& do_problem,
+    const std::string& file_base,
+    const std::vector<std::string>& bndry_surfs,
+    const std::vector<std::pair<std::string, std::pair<std::string, double>>>& int_surfs);
+
+  /**
+   * Read a surface angular flux vector from a file.
+   *
+   * \param do_problem Discrete ordinates problem
+   * \param file_base File name base
+   * \param surfaces Surface names to read
+   */
+  static std::vector<SurfaceAngularFlux>
+  ReadSurfaceAngularFluxes(DiscreteOrdinatesProblem& do_problem,
+                           const std::string& file_base,
+                           const std::vector<std::string>& surfaces);
 };
 
 } // namespace opensn
