eappen-nelluvelil/opensn-eappen

CBC FLUDS uses minimally sized local psi banks

modules/linear_boltzmann_solvers/discrete_ordinates_problem/sweep/fluds/cbc_fluds_common_data.cc
@@ -165,6 +165,38 @@ CBC_FLUDSCommonData::CBC_FLUDSCommonData(
       }
     }
   }
+
+  total_local_face_slot_nodes_ = cbc_spds.GetTotalLocalFaceSlotNodes();
+  local_face_slot_node_offsets_by_face_.assign(num_local_faces, INVALID_FACE_SLOT);
+  const auto& slot_ids = cbc_spds.GetLocalFaceSlotIDs();
+  const auto& slot_node_offsets = cbc_spds.GetLocalFaceSlotNodeOffsets();
+
+  for (const auto& cell : grid.local_cells)
+  {
+    const auto face_offset = face_offsets_[cell.local_id];
+    for (std::size_t f = 0; f < cell.faces.size(); ++f)
+    {
+      const auto& face = cell.faces[f];
+      if ((not face.has_neighbor) or (not face.IsNeighborLocal(&grid)))
+        continue;
+
+      const auto orientation = face_orientations[cell.local_id][f];
+      std::uint32_t face_task_id = CBC_SPDS::INVALID_LOCAL_FACE_TASK_ID;
+      if (orientation == FaceOrientation::OUTGOING)
+        face_task_id =
+          cbc_spds.GetOutgoingLocalFaceTaskID(cell.local_id, static_cast<unsigned int>(f));
+      else if (orientation == FaceOrientation::INCOMING)
+        face_task_id =
+          cbc_spds.GetIncomingLocalFaceTaskID(cell.local_id, static_cast<unsigned int>(f));
+
+      if (face_task_id == CBC_SPDS::INVALID_LOCAL_FACE_TASK_ID)
+        continue;
+
+      const auto slot_id = slot_ids[face_task_id];
+      local_face_slot_node_offsets_by_face_[face_offset + f] =
+        static_cast<std::size_t>(slot_node_offsets[slot_id]);
+    }
+  }
 }
 
 void
@@ -320,6 +352,13 @@ CBC_FLUDSCommonData::IncomingFaceCell(std::size_t incoming_face_slot) const
 {
   CheckFinalized();
   return incoming_face_cells_[incoming_face_slot];
+}
+
+std::size_t
+CBC_FLUDSCommonData::LocalFaceSlotNodeOffset(std::uint32_t cell_local_id,
+                                             unsigned int face_id) const noexcept
+{
+  return local_face_slot_node_offsets_by_face_[face_offsets_[cell_local_id] + face_id];
 }
 
 } // namespace opensn

Open-Sn/opensn

modules/linear_boltzmann_solvers/discrete_ordinates_problem/sweep/fluds/cbc_fluds_common_data.cc
@@ -165,6 +165,38 @@ CBC_FLUDSCommonData::CBC_FLUDSCommonData(
       }
     }
   }
+
+  total_local_face_slot_nodes_ = cbc_spds.GetTotalLocalFaceSlotNodes();
+  local_face_slot_node_offsets_by_face_.assign(num_local_faces, INVALID_FACE_SLOT);
+  const auto& slot_ids = cbc_spds.GetLocalFaceSlotIDs();
+  const auto& slot_node_offsets = cbc_spds.GetLocalFaceSlotNodeOffsets();
+
+  for (const auto& cell : grid.local_cells)
+  {
+    const auto face_offset = face_offsets_[cell.local_id];
+    for (std::size_t f = 0; f < cell.faces.size(); ++f)
+    {
+      const auto& face = cell.faces[f];
+      if ((not face.has_neighbor) or (not face.IsNeighborLocal(&grid)))
+        continue;
+
+      const auto orientation = face_orientations[cell.local_id][f];
+      std::uint32_t local_face_id = CBC_SPDS::INVALID_LOCAL_FACE_ID;
+      if (orientation == FaceOrientation::OUTGOING)
+        local_face_id =
+          cbc_spds.GetOutgoingLocalFaceID(cell.local_id, static_cast<unsigned int>(f));
+      else if (orientation == FaceOrientation::INCOMING)
+        local_face_id =
+          cbc_spds.GetIncomingLocalFaceID(cell.local_id, static_cast<unsigned int>(f));
+
+      if (local_face_id == CBC_SPDS::INVALID_LOCAL_FACE_ID)
+        continue;
+
+      const auto slot_id = slot_ids[local_face_id];
+      local_face_slot_node_offsets_by_face_[face_offset + f] =
+        static_cast<std::size_t>(slot_node_offsets[slot_id]);
+    }
+  }
 }
 
 void
@@ -320,6 +352,13 @@ CBC_FLUDSCommonData::IncomingFaceCell(std::size_t incoming_face_slot) const
 {
   CheckFinalized();
   return incoming_face_cells_[incoming_face_slot];
+}
+
+std::size_t
+CBC_FLUDSCommonData::LocalFaceSlotNodeOffset(std::uint32_t cell_local_id,
+                                             unsigned int face_id) const noexcept
+{
+  return local_face_slot_node_offsets_by_face_[face_offsets_[cell_local_id] + face_id];
 }
 
 } // namespace opensn